7bf74319f1c67c66df72d6663fd9cc28835f659f
nil <nil@nils-MacBook-Pro.local>
2015-11-22 21:12:03 +0800

    daemon

---
+++ becomeDaemon.hpp
@@ -0,0 +1,12 @@
+#ifndef _become_daemon_hpp_
+#define _become_daemon_hpp_
+
+#define BD_NO_CHDIR 01
+#define BD_NO_CLODE_FILES 02
+#define BD_NO_REOPEN_STD_FDS 04
+
+#define BD_NO_UMASKO 010
+
+#define BD_MAX_CLOSE 8192
+
+#endif // _become_daemon_hpp_
