964a8e5ae52bd2f4a50e367be25f1eac62b81a48
linxiaobo2 <linxiaobo2@kingsoft.com>
2016-02-04 21:23:19 +0800

    0

--- CollectiveIntelligence/recommandations.py
+++ CollectiveIntelligence/recommandations.py
@@ -3,12 +3,15 @@ from math import sqrt
 # A dictionary of movie critics add their ratings of a small
 # set of movies
 critics = {
-    'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 'The Night Listener': 3.0},
+    'Lisa Rose': {'Lady in the Water': 2.5,
+                  'Snakes on a Plane': 3.5,
+                  'Just My Luck': 3.0,
+                  'Superman Returns': 3.5,
+                  'You, Me and Dupree': 2.5,
+                  'The Night Listener': 3.0},
     'Gene Seymour': {},
 
 }
-
-
 
 def sim_distance(prefs, person1, person2):
     si = {}
@@ -26,3 +29,31 @@ def sim_distance(prefs, person1, person2):
 
     return 1 / (1 + sum_of_squares)
 
+def sim_pearson(prefs, p1, p2):
+    si = {}
+    for item in prefs[p1]:
+        if item in prefs[p2]:
+            si[item] = 1
+
+    n = len(si)
+
+    if n == 0:
+        return 0
+
+    sum1 = sum([prefs[p1][it] for it in si])
+    sum2 = sum([prefs[p2][it] for it in si])
+
+    sum1Sq = sum([pow(prefs[p1][it],2) for it in si])
+    sum2Sq = sum([pow(prefs[p2][it],2) for it in si])
+
+    pSum = sum([prefs[p1][it]*prefs[p2][it] for it in si])
+
+    num = pSum-(sum1*sum2/n)
+    den = sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+    if den == 0:
+        return 0
+
+    r = num/den
+    return r
+
+    
